fe835f27575613888d3ea132abfdb6147b9eb2d3
khion <nick_gayem@yahoo.com>
2021-10-04 10:21:00 -0700

    added CausesActivity, PreventionActivity and ContributeActivity

--- app/src/main/java/ca/bcit/androidproject/InformationActivity.java
+++ app/src/main/java/ca/bcit/androidproject/InformationActivity.java
@@ -2,7 +2,9 @@ package ca.bcit.androidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class InformationActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class InformationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
+    }
+
+    public void onCausesClick(View view) {
+        Intent i = new Intent(this, CausesActivity.class);
+        startActivity(i);
+    }
+
+    public void onPreventionClick(View view) {
+        Intent i = new Intent(this, PreventionActivity.class);
+        startActivity(i);
+    }
+
+    public void onContributeClick(View view) {
+        Intent i = new Intent(this, ContributeActivity.class);
+        startActivity(i);
     }
 }
