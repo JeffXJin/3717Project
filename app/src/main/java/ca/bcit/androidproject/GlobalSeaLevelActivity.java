bfcf6ae2f0d654b04daa4a6a91d1ecdbcfdd482d
JeffXJin <JeffXJin@gmail.com>
2021-10-06 14:33:42 -0700

    Added maps feature

---
+++ app/src/main/java/ca/bcit/androidproject/GlobalSeaLevelActivity.java
@@ -0,0 +1,14 @@
+package ca.bcit.androidproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GlobalSeaLevelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_global_sea_level);
+    }
+}
